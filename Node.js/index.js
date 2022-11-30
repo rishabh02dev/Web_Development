7d4d376cec610055f45ff486684c62ccd1826b87
Rishabh Shukla <kumkumshukla631@gmail.com>
2022-11-30 23:21:47 +0530

    updating_node.js

---
+++ Node.js/index.js
@@ -0,0 +1,5 @@
+// const average = require("./mod");
+const mod = require("./mod");
+console.log(mod.name)
+console.log(mod.avg([3,4]));
+console.log("This is index.js");
---
+++ Node.js/mod.js
@@ -0,0 +1,15 @@
+console.log("his is module");
+function average (arr){
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element;
+    });
+    return sum/arr.length;
+}
+
+module.exports = {
+    avg : average,
+    name : "harry",
+    repo : "Github"
+}
+module.exports.name = "rishabh";
