43569c9facf45929e6df23df1161da92e44e3c14
Rishabh Shukla <kumkumshukla631@gmail.com>
2022-11-30 23:19:45 +0530

    updating node.js

---
+++ Node.js/npm.js
@@ -0,0 +1,6 @@
+console.log("THis is tutorial 69");
+//this is used to  handle modules in javascript
+//1.3.5 major minor patch
+console.log("rishabh shukla hemloo ram ram choorii")
+//^ it makes sure that the exact version will be downloaded
+// - it install these as well as the other latest versions.
