b9a42ab802eccdac5b3b1d9e03007afd3b272a7e
Rishabh Shukla <kumkumshukla631@gmail.com>
2023-03-10 23:30:04 +0530

    Classes & Objects

---
+++ HTML/Javascript files/classes&objects.js
@@ -0,0 +1,61 @@
+// const y = {
+//     name: "Anuj",
+//     age: 26,
+//     hobbies:['coding' , 'eating', 'cycling'],
+//     foods:{
+//         Sunday : "pizza",
+//         monday : "Burger"
+//     }
+// }
+// y.name = "Shivam";
+// y.isHuman = true;
+// console.log(y);
+
+// const foodsKey = "foods";
+
+// myObj.skill = "Coding";
+// myObj.name = "Anuj";
+
+// console.log(myObj["name"]);
+
+// for (const key in myObj) {
+//   //   console.log(myObj[key]);
+// }
+
+// function getVehicle() {
+//   return {
+//     type: "car",
+//     wheels: 4,
+//   };
+// }
+
+// console.log(typeof getVehicle);
+
+//object destructing ES6
+
+// console.log(myObj);
+// const myObj = {
+//   age: 26,
+//   isHuman: true,
+//   hobbies: ["singing", "coding"],
+// };
+
+// const { age: myAge, isHuman: myIsHuman } = myObj;
+
+// console.log(myAge, myIsHuman);
+
+// function shouting() {
+//   console.log("I am shouting");
+// }
+
+// // talking(shouting);
+// talking(shouting);
+
+// function talking(shouting) {
+//   console.log("I am talking");
+//   shouting();
+// }
+
+
+//this keyword in Javascript
+
---
+++ HTML/Javascript files/classes.js
@@ -0,0 +1,35 @@
+//creating a class
+
+// class Vehicle {
+//   constructor(model, wheels) {
+//     this.model = model;
+//     this.wheels = wheels;
+//   }
+
+//   start(){
+//     console.log('vehicle starting');
+//   }
+
+// }
+
+
+// //In javascript classes are just a modified version of functions
+// const Obj1 = new Vehicle("Sedan", 6);
+// const Obj2 = new Vehicle("Hatchback" , 4)
+// console.log(Obj1);
+// console.log(Obj2);
+// Obj1.start();
+
+// const student = {
+//   name: "Anuj",
+//   marks: 56,
+//   talk: function () {
+//     console.log(`my marks are ${this.name}`);
+//   },
+// };
+
+// student.talk();
+
+
+//Learning JSON in Javascript
+
