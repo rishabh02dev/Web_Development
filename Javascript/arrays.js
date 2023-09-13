0ac95446630bb569671f42e94462f0e750165a36
Rishabh Shukla <kumkumshukla631@gmail.com>
2023-09-13 15:04:07 +0530

    Arrays

---
+++ Javascript/arrays.js
@@ -0,0 +1,27 @@
+// let arr = [12,33,44,10];
+// let arr2 = ["abhi" , "singh"];
+// let arr3 = [12,"abhi",33,"singh",[1,2]];
+// console.log(arr3);
+
+// let arr4 = [];
+// arr[0] = 12;
+// arr[1] = 1;
+// arr[2] = 11;
+// arr[3] = 14;
+// arr[4] = 13;
+// console.log(arr4);
+
+let arr5 = [];
+
+for(let i =0; i<=10; i++){
+    arr5[i] = "abhi";
+}
+console.log(arr5);
+
+let myObj = {name:"Abhi"
+            , surname:"Singh",
+            income:"2000",
+        male:"true"};
+
+myObj.newData = "Beta";
+console.log(myObj);
