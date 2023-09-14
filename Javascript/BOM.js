57d5646bf3dfe2b42471d10d73b74d75d1364514
Rishabh Shukla <kumkumshukla631@gmail.com>
2023-09-15 00:46:49 +0530

    BOM.js

---
+++ Javascript/BOM.js
@@ -0,0 +1,11 @@
+console.log(location.protocol);
+console.log(location.hostname);
+console.log(location.port);
+console.log(location.pathname);
+// console.log(location.assign("https://youtube.con/6packprogrammer"));
+
+// console.log(location.replace("htttps://instagram.com/shukla.rishabh_"));
+
+//Assign it basically assigns a new page that's why it offers a back page navigation feature.
+
+//while replace just replaces that current page to the required page and it doesn't offer a back page navigation feature.
